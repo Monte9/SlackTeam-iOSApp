e5e111d61245c8e87f262e3541cb421f8fadae85
Monte9 <Monte9@users.noreply.github.com>
2016-09-15 20:25:43 -0700

    xcode init

---
+++ SlackTeam/SlackTeam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlackTeam
+//
+//  Created by Monte with Pillow on 9/15/16.
+//  Copyright © 2016 Monte Thakkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
