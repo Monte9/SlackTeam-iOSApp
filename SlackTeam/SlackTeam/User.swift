2e1871a8dec0a90d4819e8d082eb5f0cd63e151f
Monte9 <Monte9@users.noreply.github.com>
2016-09-17 00:15:12 -0700

    realm db working

--- SlackTeam/SlackTeam/User.swift
+++ SlackTeam/SlackTeam/User.swift
@@ -2,61 +2,25 @@
 //  User.swift
 //  SlackTeam
 //
-//  Created by Monte with Pillow on 9/15/16.
+//  Created by Monte with Pillow on 9/16/16.
 //  Copyright © 2016 Monte Thakkar. All rights reserved.
 //
 
-import UIKit
+import Foundation
+import RealmSwift
 
-class User: NSObject {
+class User: Object {
     
-    var imageOriginalUrl: NSURL?
-    var image32Url: NSURL?
-    var firstName: String?
-    var lastName: String?
-    var realName: String?
-    var title: String?
-    var skype: String?
-    var phone: String?
-    var userName: String?
-    var email: String?
-    var timezone: String?
+    dynamic var imageOriginalUrl: String? = nil
+    dynamic var image32Url: String? = nil
+    dynamic var firstName: String? = nil
+    dynamic var lastName: String? = nil
+    dynamic var realName: String? = nil
+    dynamic var title: String? = nil
+    dynamic var skype: String? = nil
+    dynamic var phone: String? = nil
+    dynamic var userName: String? = nil
+    dynamic var email: String? = nil
+    dynamic var timezone: String? = nil
     
-    init(dictionary: NSDictionary) {
-        
-        self.userName = dictionary["name"] as? String
-        self.timezone = dictionary["tz_label"] as? String
-        
-        if let profile = dictionary["profile"] as? NSDictionary {
-            if let imageOriginalString = profile["image_original"] as? String {
-                imageOriginalUrl = NSURL(string: imageOriginalString)!
-            } else {
-                imageOriginalUrl = nil
-            }
-            
-            if let image32String = profile["image_32"] as? String {
-                image32Url = NSURL(string: image32String)!
-            } else {
-                image32Url = nil
-            }
-            
-            self.firstName = profile["first_name"] as? String
-            self.lastName = profile["last_name"] as? String
-            self.realName = profile["real_name"] as? String
-            self.title = profile["title"] as? String
-            self.skype = profile["skype"] as? String
-            self.phone = profile["phone"] as? String
-            self.email = profile["email"] as? String
-        }
-    }
-    
-    class func users(array: [NSDictionary]) -> [User] {
-        var users = [User]()
-        for dictionary in array {
-            let user = User(dictionary: dictionary)
-            users.append(user)
-        }
-        return users
-    }
-
 }
